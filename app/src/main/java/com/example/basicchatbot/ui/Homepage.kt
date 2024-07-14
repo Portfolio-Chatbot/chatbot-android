7661fd2ab4c63f0196e53867a65da3f5eb757cde
David <david.yudaputra@gmail.com>
2024-07-14 09:53:50 +0700

    add background

--- app/src/main/java/com/example/basicchatbot/ui/Homepage.kt
+++ app/src/main/java/com/example/basicchatbot/ui/Homepage.kt
@@ -1,21 +1,18 @@
 package com.example.basicchatbot.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.basicchatbot.R
+import com.example.basicchatbot.databinding.ActivityMainBinding
 
 class Homepage : AppCompatActivity() {
+    companion object {
+        private val TAG = Homepage::class.java.simpleName
+    }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_homepage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
